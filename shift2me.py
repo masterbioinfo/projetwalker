524ebb1a262121f47726f06cf89296a0b11463b9
Louis <ls.duchemin@gmail.com>
2017-11-05 17:55:18 +0100

    Improved usage string for docopt

--- shift2me.py
+++ shift2me.py
@@ -5,10 +5,11 @@ Shift2Me Project - Dating site for proteins !!!
 
 Usage: 
 	shift2me.py [-c <cutoff>] <file.list> <file.list> ... 
-	shift2me.py [-c <cutoff>] <dir> 
-
+	shift2me.py [-c <cutoff>] ( <dir> | <saved_job> )
+	shift2me.py -h
 Options:
   -c <cutoff> --cut-off=<cutoff>  Set default cutoff at <cutoff> (float).
+  -h --help  Print help and usage
 
 This program can calculate chemicals shifts of 15N and 1H during a portein protein interaction in fonction of titation of the secondary protein. 
 He generate plots to show chemicals shifts for each titration of the secondary protein. You can fix a cutoff to appreciate residus involved in protein protein interaction. 
