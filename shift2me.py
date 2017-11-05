27042d06d811c0531a78de2aa4670821bb8f1935
Louis <ls.duchemin@gmail.com>
2017-11-05 17:46:11 +0100

    Fixed bad docopt configuration

--- shift2me.py
+++ shift2me.py
@@ -4,11 +4,11 @@
 Shift2Me Project - Dating site for proteins !!!
 
 Usage: 
-	shift2me.py [ -c <cutoff> ] <file.list> <file.list> ... 
-	shift2me.py [ -c <cutoff> ] <dir> 
+	shift2me.py [-c <cutoff>] <file.list> <file.list> ... 
+	shift2me.py [-c <cutoff>] <dir> 
 
 Options:
-	-c <cutoff>, --cut-off=<cutoff>  Set default cutoff at <cutoff> (float).
+  -c <cutoff> --cut-off=<cutoff>  Set default cutoff at <cutoff> (float).
 
 This program can calculate chemicals shifts of 15N and 1H during a portein protein interaction in fonction of titation of the secondary protein. 
 He generate plots to show chemicals shifts for each titration of the secondary protein. You can fix a cutoff to appreciate residus involved in protein protein interaction. 
@@ -31,12 +31,12 @@ from classes.command import ShiftShell
 
 
 args = docopt(__doc__)
-
+print(args)
 #Enter experimentals files :
 if args["<file.list>"]:
-	titration = Titration(args["<file.list>"], cutOff=args["<cutoff>"])
+	titration = Titration(args["<file.list>"], cutOff=args["--cut-off"])
 elif args["<dir>"]:
-	titration = Titration(args["<dir>"], cutOff=args["<cutoff>"])
+	titration = Titration(args["<dir>"], cutOff=args["--cut-off"])
 
 #Cutoff selection by the user.
 plt.ioff()
