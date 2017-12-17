07696f9d79ac41e58395dc9265bed9477e54f26b
Louis <ls.duchemin@gmail.com>
2017-12-17 17:31:51 +0100

    Added base widgets + protocole control widgets in ipywidgets.py (to be renamed and organized) Updated CSS and requirements.txt

--- classes/ipywidgets.py
+++ classes/ipywidgets.py
@@ -1,4 +1,4 @@
-from ipywidgets import Box, HBox, VBox, BoundedFloatText, Button, Text, Widget, Label, Layout
+from ipywidgets import Box, HBox, VBox, BoundedFloatText, Button, Text, Widget, Label, Layout, HTML
 
 class TitrationWidget(Widget):
     "Root class for titration GUI elements"
@@ -252,9 +252,192 @@ class StartParamContainer(TitrationWidget, VBox):
         self.children = ( titrationName, MoleculesContainer(),analyteStartVol, startVol)
 
 
-class VolumesContainer(TitrationWidget, VBox):
+class VolumeWidget(TitrationWidget, BoundedFloatText, WidgetKwargs):
 
-    def __init__(self, *args, **kwargs):
-        VBox.__init__(self, *args, **kwargs)
+    widget_kw = {
+        'min': 0,
+        'max': 100000,
+        "layout" : Layout(width='100%', height='28px'),
+        'style' : {'description_width':'50%'}
+    }
+
+    def __init__(self, step_id, *args, **kwargs):
+        self.step_id = int(step_id)
+        type(self).update(kwargs)
+        kwargs['description'] = "Step {number:d}".format(number=self.step_id)
+        BoundedFloatText.__init__(self, *args,**kwargs)
         TitrationWidget.__init__(self)
 
+
+
+class PanelContainer(VBox):
+
+    HeaderBox = VBox
+    ContentBox = VBox
+    FooterBox = VBox
+
+    def __init__(self, heading=[], content=[], footer=None, *args, **kwargs):
+        VBox.__init__(self, *args, **kwargs)
+
+        self._dom_classes += ('panel', 'panel-default')
+
+        self.heading = self.HeaderBox(heading)
+        self.content = self.ContentBox(content)
+        self.footer = None
+
+        if footer is not None:
+            self.add_footer(footer)
+
+        self.heading._dom_classes += ("panel-heading",)
+        self.content._dom_classes += ('panel-body',)
+
+        if self.footer:
+            self.children = (self.heading, self.content, self.footer)
+        else:
+            self.children = (self.heading, self.content)
+
+
+    def add_footer(self, widget_list=[]):
+        self.footer=self.FooterBox(widget_list)
+        self.footer._dom_classes += ('panel-footer',)
+        self.children = (self.heading, self.content, self.footer)
+
+    def set_heading(self, widget_list):
+        self.heading.children = widget_list
+
+    def set_content(self, widget_list):
+        self.content.children = widget_list
+
+    def set_footer(self, widget_list):
+        self.footer.children = widget_list
+
+
+class ProtocolePanel(TitrationWidget, PanelContainer):
+
+    def __init__(self, *args, **kwargs):
+        self.title = Label('Protocole')
+        self.title._dom_classes += ('panel-header-title',)
+
+        PanelContainer.__init__(self, [self.title],  *args, **kwargs)
+        TitrationWidget.__init__(self)
+
+        self.update()
+
+    def update(self):
+        self.protocole = HTML(self.titration.make_protocole(index=False).to_html(index=False))
+        self.protocole._dom_classes += ('rendered_html', 'protocole-table')
+        self.set_content([self.protocole])
+
+
+
+class VolumePanel(TitrationWidget, PanelContainer):
+
+    def __init__(self, *args, **kwargs):
+        PanelContainer.__init__(self, *args, **kwargs)
+        TitrationWidget.__init__(self)
+
+        self.steps = 0
+        self._dom_classes += ('volumes-container',)
+
+        self.add_button = Button(
+            description = 'Add',
+            button_style='primary',
+            layout=Layout(width='50%'))
+        self.add_button.on_click(self.add_volume)
+
+        self.remove_button = Button(
+            description='Remove',
+            button_style='warning',
+            layout=Layout(width='50%'),
+            disabled=True)
+        self.remove_button.on_click(self.remove_volume)
+
+        self.buttons = HBox(
+            [self.add_button, self.remove_button],
+            layout=Layout(
+                padding="5px 5px 5px 5px"))
+
+        self.validate_button = Button(
+            description='Validate',
+            button_style='success',
+            layout=Layout(width='100%'),
+            disabled=False)
+        self.validate_button.on_click(self.send_volumes)
+
+        self.volumesBox = VBox(
+            [],
+            layout=Layout(
+                display='flex',
+                flex_flow="column",
+                padding='5px 5px 5px 5px'))
+
+        formLabel = Label("Added titrant volumes")
+        formLabel._dom_classes += ('panel-header-title',)
+
+        self.set_heading([formLabel, self.buttons])
+        self.set_content([self.volumesBox])
+        self.add_footer([self.validate_button])
+
+        self.volWidgets = []
+        for volume in self.titration.volumes:
+            self.add_volume()
+
+
+
+    def update_children(self):
+        self.volumesBox.children = self.volWidgets
+        self.set_content([self.volumesBox])
+        # protocole = HTML(self.titration.make_protocole(index=False).to_html(index=False))
+
+        # protocole._dom_classes += ('rendered_html', )
+        # self.children = (
+        #     self.form,
+        #     protocole)
+
+    def send_volumes(self, button):
+        self.titration.set_volumes(self.volumes)
+        self.update_children()
+
+    def add_volume(self, button=None, disabled=False):
+        if self.steps == 0:
+            disabled = True
+        else:
+            self.remove_button.disabled = False
+
+        if self.steps < self.titration.steps:
+            value = self.titration.volumes[self.steps]
+        else:
+            value = 0
+        self.volWidgets.append(
+            VolumeWidget(self.steps, value=value, disabled=disabled)
+        )
+
+        self.update_children()
+        self.steps += 1
+
+    def remove_volume(self, button):
+        if len(self.volWidgets) > 1:
+            vol =  self.volWidgets.pop()
+            del vol
+            self.update_children()
+            self.steps -= 1
+        if self.steps <= 1:
+            self.remove_button.disabled = True
+
+
+    @property
+    def volumes(self):
+        return [widget.value for widget in self.volWidgets]
+
+
+class ProtocoleContainer(HBox):
+
+    def __init__(self, *args, **kwargs):
+        HBox.__init__(self, *args, **kwargs, layout=Layout(justify_content='space-around'))
+        self.volumes = VolumePanel(layout=Layout(width='250px'))
+        self.volumes.validate_button.on_click(self.on_submit)
+        self.protocole = ProtocolePanel()
+        self.children = (self.volumes, self.protocole)
+
+    def on_submit(self, button):
+        self.protocole.update()
